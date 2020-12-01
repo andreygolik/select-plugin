5d5d06f2d6679e169b31ba9dd7f9004655dad4e8
Andrey Golik <1858191+andreygolik@users.noreply.github.com>
2020-12-01 18:11:18 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import './styles.scss'
 import { Select } from './select'
 
-const select = new Select()
+const select = new Select('#select', {
+  placeholder: 'Please select element',
+  // selectedId: '1',
+  data: [
+    { id: '1', value: 'Angular' },
+    { id: '2', value: 'React' },
+    { id: '3', value: 'Vue' },
+    { id: '5', value: 'Node' },
+    { id: '6', value: 'Deno' },
+    { id: '4', value: 'Express' },
+  ],
+  onSelect(item) {
+    console.log('Selected Item:', item)
+  },
+})
+
+// debug
+window.s = select
--- src/select/select.js
+++ src/select/select.js
@@ -1,1 +1,110 @@
-export class Select {}
+const getTemplate = (options = {}) => {
+  const { data = [], selectedId } = options
+  let { placeholder: value = '' } = options
+
+  const items = data.map((item) => {
+    let selected = ''
+
+    if (item.id === selectedId) {
+      value = item.value
+      selected = 'selected'
+    }
+
+    return `
+      <li class="select__item ${selected}" data-type="item" data-id="${item.id}">${item.value}</li>
+    `
+  })
+
+  return `
+    <div class="select__backdrop" data-type="backdrop"></div>
+    <div class="select__input" data-type="input">
+      <span data-type="value">${value}</span>
+      <i class="fa fa-chevron-down" data-type="arrow"></i>
+    </div>
+    <div class="select__dropdown">
+      <ul class="select__list">
+        ${items.join('')}
+      </ul>
+    </div>
+  `
+}
+
+export class Select {
+  constructor(selector, options) {
+    this.$el = document.querySelector(selector)
+    this.options = options
+    this.selectedId = options.selectedId
+
+    this.#render()
+    this.#setup()
+  }
+
+  #render() {
+    const { data, placeholder, selectedId } = this.options
+    this.$el.classList.add('select')
+    this.$el.innerHTML = getTemplate({ data, placeholder, selectedId })
+  }
+
+  #setup() {
+    this.clickHandler = this.clickHandler.bind(this)
+    this.$el.addEventListener('click', this.clickHandler)
+    this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+    this.$value = this.$el.querySelector('[data-type="value"]')
+  }
+
+  clickHandler(event) {
+    const { type } = event.target.dataset
+
+    if (type === 'input') {
+      this.toggle()
+    } else if (type === 'item') {
+      const { id } = event.target.dataset
+      this.select(id)
+    } else if (type === 'backdrop') {
+      this.close()
+    }
+  }
+
+  get isOpen() {
+    return this.$el.classList.contains('open')
+  }
+
+  get current() {
+    return this.options.data.find((item) => item.id === this.selectedId)
+  }
+
+  select(id) {
+    this.selectedId = id
+    this.$value.textContent = this.current.value
+
+    this.$el.querySelectorAll('[data-type="item"].selected').forEach((el) => {
+      el.classList.remove('selected')
+    })
+    this.$el.querySelector(`[data-id="${id}"`).classList.add('selected')
+
+    this.options.onSelect ? this.options.onSelect(this.current) : null
+
+    this.close()
+  }
+
+  toggle() {
+    this.isOpen ? this.close() : this.open()
+  }
+
+  open() {
+    this.$el.classList.add('open')
+    this.$arrow.classList.remove('fa-chevron-down')
+    this.$arrow.classList.add('fa-chevron-up')
+  }
+
+  close() {
+    this.$el.classList.remove('open')
+    this.$arrow.classList.remove('fa-chevron-up')
+    this.$arrow.classList.add('fa-chevron-down')
+  }
+
+  destroy() {
+    this.$el.removeEventListener('click', this.clickHandler)
+    this.$el.innerHTML = ''
+  }
+}
